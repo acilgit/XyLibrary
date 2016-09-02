fc6e744333c77454f21704525d1dae87cfb04c15
xiuge <acil@163.com>
2016-09-02 14:55:53 +0800

    add permission

---
+++ xylibrary/src/main/java/com/xycode/xylibrary/interfaces/Interfaces.java
@@ -0,0 +1,12 @@
+package com.xycode.xylibrary.interfaces;
+
+/**
+ * Created by XY on 2016-09-02.
+ */
+public class Interfaces {
+
+    public interface OnStringData<T> {
+        String getDataString(T data);
+    }
+
+}
