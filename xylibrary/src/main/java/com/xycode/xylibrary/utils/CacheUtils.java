ad23ff95b57ca05505450382cfc11b4be16864b0
berlin <869459739@qq.com>
2016-10-21 16:36:39 +0800

    add cacheUtils

---
+++ xylibrary/src/main/java/com/xycode/xylibrary/utils/CacheUtils.java
@@ -0,0 +1,134 @@
+package com.xycode.xylibrary.utils;
+
+import android.content.Context;
+import android.os.Environment;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipeline;
+
+import java.io.File;
+import java.math.BigDecimal;
+
+/**缓存工具类
+ * Created by Administrator on 2016/10/18 0018.
+ */
+
+public class CacheUtils {
+    /**
+     * 获取缓存大小
+     * @return
+     * @throws Exception
+     */
+    public static String getTotalCacheSize(Context context){
+        long cacheSize = 0;
+        try {
+            cacheSize = getFolderSize(context.getCacheDir());
+            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                cacheSize += getFolderSize(context.getExternalCacheDir());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return formatUnit(cacheSize);
+    }
+
+    /**
+     * 清除缓存
+     * @param context
+     */
+    public static void clearAllCache(Context context) {
+        deleteDir(context.getCacheDir());
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            deleteDir(context.getExternalCacheDir());
+        }
+    }
+
+    public static void clearDiskAndFrscoCache(Context context){
+        clearAllCache(context);
+        clearFrescoCache();
+    }
+
+    private static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        return dir.delete();
+    }
+
+    /**
+     * 清楚fresco缓存
+     */
+    public static void clearFrescoCache(){
+        ImagePipeline imagePipeline = Fresco.getImagePipeline();
+        imagePipeline.clearMemoryCaches();
+        imagePipeline.clearDiskCaches();
+        imagePipeline.clearCaches();
+    }
+
+
+    /**
+     *  获取文件大小
+     *Context.getExternalFilesDir() --> SDCard/Android/data/你的应用的包名/files/ 目录，一般放一些长时间保存的数据
+     *Context.getExternalCacheDir() --> SDCard/Android/data/你的应用包名/cache/目录，一般存放临时缓存数据
+     * @param file
+     * @return
+     * @throws Exception
+     */
+    public static long getFolderSize(File file) throws Exception {
+        long size = 0;
+        try {
+            File[] fileList = file.listFiles();
+            for (int i = 0; i < fileList.length; i++) {
+                // 如果下面还有文件
+                if (fileList[i].isDirectory()) {
+                    size = size + getFolderSize(fileList[i]);
+                } else {
+                    size = size + fileList[i].length();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return size;
+    }
+
+
+    /**
+     * 格式化单位
+     * @param cacheSize
+     * @return
+     */
+    public static String formatUnit(long cacheSize){
+        double kiloByte = cacheSize / 1024;
+        if (kiloByte < 1){
+            return "OK";
+        }
+        double megaByte = kiloByte / 1024;
+        if (megaByte < 1) {
+            BigDecimal result = new BigDecimal(Double.toString(kiloByte));
+            return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "K";
+        }
+        double gigaByte = megaByte / 1024;
+        if (gigaByte < 1) {
+            BigDecimal result = new BigDecimal(Double.toString(megaByte));
+            return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "M";
+        }
+
+        double teraByte = gigaByte / 1024;
+        if (teraByte < 1) {
+            BigDecimal result = new BigDecimal(Double.toString(megaByte));
+            return result.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "GB";
+        }
+
+        BigDecimal result4 = new BigDecimal(teraByte);
+        return result4.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString()
+                + "TB";
+    }
+}
